a05da89359c09addeebff9f74c9e9d05738d5e89
abass.bayo <abass.bayo@jet.com>
2019-08-05 03:39:02 -0400

    clean up history branch

--- app/src/main/java/com/abasscodes/githubklient/settings/UserSettings.kt
+++ app/src/main/java/com/abasscodes/githubklient/settings/UserSettings.kt
@@ -1,9 +1,6 @@
 package com.abasscodes.githubklient.settings
 
 interface UserSettings {
-//    fun storeQuery(query: String)
-//    fun getQuerySet() : Set<String>?
-
     fun storeLastQuery(query: String)
     fun getLastQuerySet() : List<String>
 }
--- app/src/main/java/com/abasscodes/githubklient/settings/UserSettingsManager.kt
+++ app/src/main/java/com/abasscodes/githubklient/settings/UserSettingsManager.kt
@@ -6,7 +6,6 @@ import androidx.preference.PreferenceManager
 
 open class UserSettingsManager(context: Context) : UserSettings {
 
-
     override fun storeLastQuery(query: String) {
         val prevValue = preferences.getString(QUERY_SET, null)
         if(prevValue == null) {
@@ -14,7 +13,6 @@ open class UserSettingsManager(context: Context) : UserSettings {
         } else {
             preferences.edit().putString(QUERY_SET, prevValue + "," + query).apply()
         }
-
     }
 
     override fun getLastQuerySet(): List<String> {
@@ -24,14 +22,6 @@ open class UserSettingsManager(context: Context) : UserSettings {
         }
         return emptyList()
     }
-
-//    override fun storeQuery(query: String) {
-//        val set = getQuerySet() ?: mutableSetOf()
-//        set.add(query)
-//        preferences.edit().putStringSet(QUERY_SET, set).apply()
-//    }
-//
-//    override fun getQuerySet(): MutableSet<String>? = preferences.getStringSet(QUERY_SET, null)
 
     private val preferences: SharedPreferences =
         PreferenceManager.getDefaultSharedPreferences(context)
