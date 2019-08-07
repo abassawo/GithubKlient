29e1cbe29585c5d299a36627be10f041b28374af
abass.bayo <abass.bayo@jet.com>
2019-08-07 05:26:31 -0400

    Make changes to order of recommended companies

--- app/src/main/java/com/abasscodes/githubklient/models/RecommendedCompany.kt
+++ app/src/main/java/com/abasscodes/githubklient/models/RecommendedCompany.kt
@@ -2,10 +2,13 @@ package com.abasscodes.githubklient.models
 
 enum class RecommendedCompany constructor(val githubName: String) {
     NYTimes("nytimes"),
-    Spotify("spotify"),
-    IntrepidPursuits("intrepidPursuits"),
-    Vimeo("vimeo"),
-    Square("square"),
     Pursuit("joinpursuit"),
-
+    Facebook("facebook"),
+    Google("google"),
+    Vimeo("vimeo"),
+    Spotify("spotify"),
+    Square("square"),
+    Venmo("venmo"),
+    Uber("uber"),
+    Lyft("lyft"),
 }
